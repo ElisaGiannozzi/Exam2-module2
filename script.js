72a22d6b2359fb42800890820d6c863f37a3d3a3
ElisaGiannozzi <elisa.giannozzi@gmail.com>
2020-05-30 16:47:09 +0200

    created select menu

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const app = new Vue ({
     el: '#app', 
     data: {
-        sources: []
+        sources: [],
+        selected: '',
+        articles: [], 
     },
+    methods: {
+        getSelected: function () {
+            let selectedSource = this.selected.id; 
+            console.log(selectedSource); 
+        }
+
+    },
+   
     created () {
-        fetch('https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/sources?language=en&apiKey=6a263bbf656948fb932c3e509acaaf7d')
+        fetch('https://newsapi.org/v2/sources?language=en&apiKey=6a263bbf656948fb932c3e509acaaf7d')
         .then(response => response.json())
         .then(json => {
             this.sources = json.sources
         })
-    }
+
+    }    
 }) 
+
+
+
 
